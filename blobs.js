603029f2b6a6841b541f9c4bfdf5f509d88b3c99
Jamie Bartlett <jamie.bartlett@uk.kps.com>
2020-05-01 11:58:43 +0100

    Initialize Express

--- blobs.js
+++ blobs.js
@@ -1,1 +1,16 @@
-console.log("TBC");
+var express = require('express');
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+//Give static access to public directory
+app.use(express.static('public'));
+
+// Root directs to game.html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/game.html');
+});
+
+http.listen(3000, () => {
+  console.log("listening on 3000");
+});
