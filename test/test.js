1034ba7caad970546027a61c8c1ba9bf18c07a51
Jamie Bartlett <jamie.bartlett@uk.kps.com>
2020-05-04 14:15:36 +0100

    Test for PlayerService

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
-var card_service = require('../service/card_service');
 
 describe('CardService', () => {
     describe('#dealCards()', () => {
+        var card_service = require('../service/card_service');
         var numberOfPlayers = Math.floor(Math.random() * 5) + 3;
         var cardsEach = Math.floor(Math.random() * 6) + 1;  
         var dealtCards = card_service.dealCards(cardsEach, numberOfPlayers);
@@ -40,4 +40,60 @@ describe('CardService', () => {
             assert.equal(duplicates.length, 0);
         })
     });
+});
+
+var { v4: uuidv4 } = require('uuid');
+describe('PlayerService', () => {
+    describe('#createPlayer()', () => {
+        var playerService = require('../service/player_service');
+        it('Players should be empty to begin with', ()=> {
+            assert.equal(playerService.getPlayers(), 0);
+        });
+        it('Should be able to create new players', () => {
+            var playersToCreate = Math.floor(Math.random() * 10) + 1;
+            for (var i = 1; i<= playersToCreate; i++){
+                var id = uuidv4();
+                playerService.createPlayer(id);
+                assert.equal(playerService.getPlayers().slice(-1)[0].socketId, id);
+            }
+            assert.equal(playerService.getPlayers().length, playersToCreate);
+        });
+    });
+    describe('#removePlayer', () => {
+        var playerService = require('../service/player_service');
+        it('Should remove players based on ID', ()=>{
+            //Create 5 players
+            for (var i=0;i<5;i++){
+                var id = uuidv4();
+                playerService.createPlayer(id);
+            }
+            //Get the number of players
+            var numberOfPlayers = playerService.getPlayers().length;
+
+            //Collect all the ids
+            var allTheIDs = playerService.getPlayers().map((player) => {return player.socketId;});
+
+            //Get a random item from the array
+            var id = allTheIDs[Math.floor(Math.random()*allTheIDs.length)];
+
+            //Remove it
+            playerService.removePlayer(id);
+
+            //Is there one less player
+            assert.equal(playerService.getPlayers().length, numberOfPlayers - 1);
+
+            //Have we removed the right one
+            assert.equal(playerService.getPlayers().includes(id), false);
+
+            //Get a list of the remaining IDs and check they are still there
+            allTheIDs.filter((value) => {
+                return value != id;
+            }).forEach((value) => {
+                assert.equal(playerService.getPlayers().filter((player) => {
+                    return player.socketId == value;
+                }).length, 1);
+            })
+        });
+        
+    })
 })
