7c30adc824004ffe3a02c4bf34248eea1e7799aa
Jamie Bartlett <jamie.bartlett@uk.kps.com>
2020-05-05 17:00:38 +0100

    Re-enable renaming

--- bootstrap/javascript/nameChangeController.js
+++ bootstrap/javascript/nameChangeController.js
@@ -1,3 +1,6 @@
 angular.module('blobs').controller("nameChangeController", ['$rootScope', '$scope', 'socket', function($rootScope, $scope, socket){
-
+    $scope.updateName = function(name){
+        socket.emit("nameChange", name);
+        $('#nameChangeModal').modal("hide");
+    }
 }]);
