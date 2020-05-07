1b9d9efe110905a31a06cdfe6a7ebdb9052f96ba
Jamie Bartlett <jamie.bartlett@uk.kps.com>
2020-05-07 10:55:56 +0100

    PLayers & Scores popup on mobile

--- service/card_service.js
+++ service/card_service.js
@@ -1,6 +1,6 @@
 var copyService = require("./copy_service");
 
-var suits = [{name:"clubs", suffix:"c"},{name:"hearts", suffix:"h"},{name:"diamonds", suffix:"d"},{name:"spades", suffix:"s"}];
+var suits = [{name:"clubs", suffix:"c", symbol: "&clubs;"},{name:"hearts", suffix:"h", symbol: "&hearts;"},{name:"diamonds", suffix:"d", symbol: "&diams;"},{name:"spades", suffix:"s", symbol: "&spades;"}];
 var values = [{label: "2", value: 2},{label: "3", value: 3},{label: "4", value: 4},{label: "5", value: 5},{label: "6", value: 6},{label: "7", value: 7},{label: "8", value: 8},
 {label: "9", value: 9},{label: "10", value: 10},{label: "jack", value: 11},{label: "queen", value: 12},{label: "king", value: 13},{label: "ace", value: 14}];
 
