26d559479aaf1a88aed3068800427245b7e94b20
Jamie Bartlett <jamie.bartlett@uk.kps.com>
2020-05-19 21:58:13 +0100

    Re-ordered cards when they are dealt

--- service/card_service.js
+++ service/card_service.js
@@ -1,7 +1,7 @@
 var copyService = require("./copy_service");
 
-var suits = [   {name:"clubs", suffix:"c", symbol: "♣", color:"black"},{name:"hearts", suffix:"h", symbol: "♥", color:"red"},
-                {name:"diamonds", suffix:"d", symbol: "♦", color:"red"},{name:"spades", suffix:"s", symbol: "♠", color:"black"}];
+var suits = [   {name:"clubs", suffix:"c", symbol: "♣", color:"black",  order: 2},{name:"hearts", suffix:"h", symbol: "♥", color:"red", order: 1},
+                {name:"diamonds", suffix:"d", symbol: "♦", color:"red", order:3},{name:"spades", suffix:"s", symbol: "♠", color:"black", order:4}];
 var values = [{label: "2", value: 2},{label: "3", value: 3},{label: "4", value: 4},{label: "5", value: 5},{label: "6", value: 6},{label: "7", value: 7},{label: "8", value: 8},
 {label: "9", value: 9},{label: "10", value: 10},{label: "jack", value: 11},{label: "queen", value: 12},{label: "king", value: 13},{label: "ace", value: 14}];
 
@@ -41,6 +41,23 @@ module.exports = {
                 //Add this card to the players hand
                 retObj.playerHands[p].push(selectedCard);
             }
+        }
+
+        //Sort the players hands
+        for  (var p=0; p<numberOfPlayers; p++){
+            retObj.playerHands[p] = retObj.playerHands[p].sort((a, b) => {
+               if (a.suit.suffix == b.suit.suffix){
+                   if (a.value < b.value){
+                       return 1;
+                   } else {
+                       return -1;
+                   }
+               } else if (a.suit.order < b.suit.order){
+                   return -1;
+               } else {
+                   return 1;
+               }    
+            });
         }
 
         //Set the trump card
